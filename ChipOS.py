7f7db220fecefb3d039746a270aa84653d212b2a
squirrelcom <84091436+squirrelcom@users.noreply.github.com>
2021-09-04 20:32:56 +0100

    Update and rename Voice Assistant (1).txt to ChipOS.py

---
+++ ChipOS.py
@@ -0,0 +1,375 @@
+from __future__ import print_function
+import warnings
+import pyttsx3
+import speech_recognition as sr
+from gtts import gTTS
+import playsound
+import os
+import datetime
+import calendar
+import random
+import wikipedia
+import webbrowser
+import ctypes
+import winshell
+import subprocess
+import pyjokes
+import smtplib
+import requests
+import json
+import time
+from twilio.rest import Client
+import wolframalpha
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from selenium import webdriver
+from time import sleep
+
+warnings.filterwarnings("ignore")
+
+engine = pyttsx3.init()
+voices = engine.getProperty("voices")
+engine.setProperty("voice", voices[1].id)
+
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
+
+def talk(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def rec_audio():
+    recog = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio = recog.listen(source)
+
+    data = " "
+
+    try:
+
+        data = recog.recognize_google(audio)
+        print("You said: " + data)
+
+    except sr.UnknownValueError:
+        print("ChipOS could not understand the audio")
+
+    except sr.RequestError as ex:
+        print("Request Error from ChipOS" + ex)
+
+    return data
+
+
+def response(text):
+    print(text)
+
+    tts = gTTS(text=text, lang="en")
+
+    audio = "Audio.mp3"
+    tts.save(audio)
+
+    playsound.playsound(audio)
+
+    os.remove(audio)
+
+
+def call(text):
+    action_call = "chip"
+
+    text = text.lower()
+
+    if action_call in text:
+        return True
+
+    return False
+
+
+def today_date():
+    now = datetime.datetime.now()
+    date_now = datetime.datetime.today()
+    week_now = calendar.day_name[date_now.weekday()]
+    month_now = now.month
+    day_now = now.day
+
+    months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ]
+
+    ordinals = [
+        "1st",
+        "2nd",
+        "3rd",
+        "4th",
+        "5th",
+        "6th",
+        "7th",
+        "8th",
+        "9th",
+        "10th",
+        "11th",
+        "12th",
+        "13th",
+        "14th",
+        "15th",
+        "16th",
+        "17th",
+        "18th",
+        "19th",
+        "20th",
+        "21st",
+        "22nd",
+        "23rd",
+        "24th",
+        "25th",
+        "26th",
+        "27th",
+        "28th",
+        "29th",
+        "30th",
+        "31st",
+    ]
+
+    return "Today is " + week_now + ", " + months[month_now - 1] + " the " + ordinals[day_now - 1] + "."
+
+
+def say_hello(text):
+    greet = ["hi", "hey", "hello"]
+
+    response = ["Welcome To ChipOS", "hello", "hey there"]
+
+    for word in text.split():
+        if word.lower() in greet:
+            return random.choice(response) + "."
+
+    return ""
+
+
+def wiki_person(text):
+    list_wiki = text.split()
+    for i in range(0, len(list_wiki)):
+        if i + 3 <= len(list_wiki) - 1 and list_wiki[i].lower() == "who" and list_wiki[i + 1].lower() == "is":
+            return list_wiki[i + 2] + " " + list_wiki[i + 3]
+
+
+
+def google_calendar():
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                '../../Voice Assistant/credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('calendar', 'v3', credentials=creds)
+
+    return service
+
+
+def calendar_events(num, service):
+    talk('Hey there! Good Day. Hope you are doing fine. These are the events to do today')
+    now = datetime.datetime.utcnow().isoformat() + 'Z'
+    print(f'Getting the upcoming {num} events')
+    events_result = service.events().list(calendarId='primary', timeMin=now,
+                                          maxResults=num, singleEvents=True,
+                                          orderBy='startTime').execute()
+    events = events_result.get('items', [])
+
+    if not events:
+        talk('No upcoming events found.')
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        events_today = (event['summary'])
+        start_time = str(start.split("T")[1].split("-")[0])
+        if int(start_time.split(":")[0]) < 12:
+            start_time = start_time + "am"
+        else:
+            start_time = str(int(start_time.split(":")[0]) - 12)
+            start_time = start_time + "pm"
+        talk(f'{events_today} at {start_time}')
+
+
+try:
+    service = google_calendar()
+    calendar_events(10, service)
+except:
+    talk("Could not connect to the local wifi network. Please try again later.")
+    exit()
+
+
+def send_email(to, content):
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.ehlo()
+    server.starttls()
+
+    server.login("email", "pass")
+    server.sendmail("email", to, content)
+    server.close()
+
+
+while True:
+
+    try:
+
+        text = rec_audio()
+        speak = ""
+
+        if call(text):
+
+            speak = speak + say_hello(text)
+
+            if "date" in text or "day" in text or "month" in text:
+                get_today = today_date()
+                speak = speak + " " + get_today
+
+            elif "time" in text:
+                now = datetime.datetime.now()
+                meridiem = ""
+                if now.hour >= 12:
+                    meridiem = "p.m"
+                    hour = now.hour - 12
+                else:
+                    meridiem = "a.m"
+                    hour = now.hour
+
+                if now.minute < 10:
+                    minute = "0" + str(now.minute)
+                else:
+                    minute = str(now.minute)
+                speak = speak + " " + "It is " + str(hour) + ":" + minute + " " + meridiem + " ."
+
+            elif "wikipedia" in text or "Wikipedia" in text:
+                if "who is" in text:
+                    person = wiki_person(text)
+                    wiki = wikipedia.summary(person, sentences=2)
+                    speak = speak + " " + wiki
+
+            elif "about" in text or "about you" in text:
+                speak = speak + """ChipOS v0.1 by Squirrel Computers."""
+
+ 
+            elif "play music" in text or "play song" in text:
+                talk("Here you go with music")
+                music_dir = r"Location of directory"
+                songs = os.listdir(music_dir)
+                d = random.choice(songs)
+                random = os.path.join(music_dir, d)
+                playsound.playsound(random)
+
+
+            elif "note" in text or "remember this" in text:
+                talk("What would you like me to write down?")
+                note_text = rec_audio()
+                note(note_text)
+                speak = speak + "I have made a note of that."
+
+            elif "tell me a joke" in text:
+                speak = speak + pyjokes.get_joke()
+
+            elif "where is" in text:
+                ind = text.lower().split().index("is")
+                location = text.split()[ind + 1:]
+                url = "https://www.google.com/maps/place/" + "".join(location)
+                speak = speak + "This is where " + str(location) + " is."
+                webbrowser.open(url)
+
+            elif "weather" in text:
+                key = "OpenWeatherMap Key"
+                weather_url = "http://api.openweathermap.org/data/2.5/weather?"
+                ind = text.split().index("in")
+                location = text.split()[ind + 1:]
+                location = "".join(location)
+                url = weather_url + "appid=" + key + "&q=" + location
+                js = requests.get(url).json()
+                if js["cod"] != "404":
+                    weather = js["main"]
+                    temperature = weather["temp"]
+                    temperature = temperature - 273.15
+                    humidity = weather["humidity"]
+                    desc = js["weather"][0]["description"]
+                    weather_response = " The temperature in Celcius is " + str(temperature) + " The humidity is " + str(
+                        humidity) + " and The weather description is " + str(desc)
+                    speak = speak + weather_response
+                else:
+                    speak = speak + "City Not Found"
+
+            elif "news" in text:
+                url = (
+                    "http://newsapi.org/v2/top-headlines?"
+                    "country= &"
+                    "apiKey= "
+                )
+
+                try:
+                    response = requests.get(url)
+                except:
+                    talk("Please check your connection")
+
+                news = json.loads(response.text)
+
+                for new in news["articles"]:
+                    print(str(new["title"]), "\n")
+                    talk(str(new["title"]))
+                    engine.runAndWait()
+
+                    print(str(new["description"]), "\n")
+                    talk(str(new["description"]))
+                    engine.runAndWait()
+                    time.sleep(2)
+
+                    
+            elif "calculate" in text:
+                app_id = "Wolfram Alpha ID"
+                client = wolframalpha.Client(app_id)
+                ind = text.lower().split().index("calculate")
+                text = text.split()[ind + 1:]
+                res = client.query(" ".join(text))
+                answer = next(res.results).text
+                speak = speak + "The answer is " + answer
+
+            elif "what is" in text or "who is" in text:
+                app_id = "Wolfram Alpha ID"
+                client = wolframalpha.Client(app_id)
+                ind = text.lower().split().index("is")
+                text = text.split()[ind + 1:]
+                res = client.query(" ".join(text))
+                answer = next(res.results).text
+                speak = speak + answer
+
+            elif "don't listen" in text or "stop listening" in text or "do not listen" in text:
+                talk("for how many seconds do you want me to sleep")
+                a = int(rec_audio())
+                time.sleep(a)
+                speak = speak + str(a) + " seconds completed. Now you can ask me anything"
+
+            elif "exit" in text or "quit" in text:
+
+            response(speak)
+
+    except:
+        talk("I don't know that")
+
